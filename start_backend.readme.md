b349fe20469840c00b2145f221182d281f1ce89b
fallmx <31561277+fallmx@users.noreply.github.com>
2024-05-31 11:53:47 +0300

    Update script for startig backend for local development

---
+++ start_backend.readme.md
@@ -0,0 +1,1 @@
+To run `start_backend.sh`, put your osm data file in the `map` folder as name `map/route-data.osm` and run `start_backend.sh`. The port 5000 must be free.
--- start_backend.sh
+++ start_backend.sh
@@ -1,8 +1,3 @@
-sudo docker run -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/car.lua /data/route-data.osm || echo "osrm-extract failed"
-
-sudo docker run -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-partition /data/route-data.osrm || echo "osrm-partition failed"
-
-sudo docker run -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-customize /data/route-data.osrm || echo "osrm-customize failed"
-
-sudo docker run -t -i -p 5000:5000 -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/route-data.osrm
-
+docker run -t --rm -v "${PWD}/map:/data" ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/car.lua /data/route-data.osm || echo "osrm-extract failed"
+docker run -t --rm -v "${PWD}/map:/data" ghcr.io/project-osrm/osrm-backend osrm-contract /data/route-data.osrm || echo "osrm-partition failed"
+docker run -t -i -p 5000:5000 -v "${PWD}/map:/data" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm ch /data/route-data.osrm
